3e104932ab581ea876c728e7f5c8e32038e786dd
Esso-ylow-noyou Pana <pns5273a@ensfsi.onmicrosoft.com>
2024-09-24 20:00:17 +0200

    <2,a tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -5,40 +5,40 @@ public class Gaulois {
 	private int force;
 	private int effetPotion = 1;
 
-	public int getEffetPotion() {
-		return effetPotion;
-	}
-
 	public Gaulois(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
-	}
-
-	public int getForce() {
-		return force;
 	}
 
 	public String getNom() {
 		return nom;
 	}
 
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + ">>");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
 	@Override
 	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+		return "Gaulois [nom=" + nom + ", force=" + force + " ]";
 	}
 
 	public static void main(String[] args) {
-		
+		Gaulois asterix =  new Gaulois("Astérix",8);
+		System.out.println(asterix.prendreParole());
+		asterix.parler("Bonjour");
+		asterix.frapper(null);
 	}
 	
-	public void parler(String texte) {
-		prendreParole();
-	}
-	public void parler(String texte) {
-		String parole = prendreParole();
-		
-	}
 	
 
 }
